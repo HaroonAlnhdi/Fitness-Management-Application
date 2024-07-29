7d8253568878c9b5fb8ce1fcd197f672ac8841e1
Haroon <you@example.com>
2024-07-29 14:15:40 +0300

    update dashbord css and add admin pages

--- controllers/controlPanel.js
+++ controllers/controlPanel.js
@@ -10,4 +10,20 @@ router.get('/dashboard', (req, res) => {
   res.render('admin/dashbord.ejs');
 });
 
+router.get('/members', (req, res) => {
+  res.render('admin/members.ejs');
+});
+
+router.get('/trainers', (req, res) => {
+  res.render('admin/trainers.ejs');
+});
+
+router.get('/admins', (req, res) => {
+  res.render('admin/admins.ejs');
+});
+
+router.get('/packages', (req, res) => {
+  res.render('admin/packages.ejs');
+});
+
 module.exports = router;
