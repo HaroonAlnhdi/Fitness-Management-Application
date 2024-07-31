24161cbabada9445c2691cc8f1edc588ae8dcf44
Haroon <you@example.com>
2024-07-31 21:31:04 +0300

    create trainer page

--- controllers/home.js
+++ controllers/home.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 const Packages = require('../models/packages.js');
-const Trainer =require("../models/trainer.js")
+const Trainer = require('../models/trainer.js');
 
 router.get('/fitness', async (req, res) => {
     const package = await Packages.find();
     res.render('home/fitness.ejs',{package});
+  });
+
+
+  router.get('/trainers', async (req, res) => {
+    const Trainers = await Trainer.find();
+    res.render('home/trainers.ejs',{Trainers});
   });
 
 
